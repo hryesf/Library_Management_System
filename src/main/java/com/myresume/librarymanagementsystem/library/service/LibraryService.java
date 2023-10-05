37b53eeefd8c24c9bdb9aa3e27eb8de44014f99f
hryesf <houriye.esfahanian@gmail.com>
2023-10-05 18:03:03 +0330

    Library methods and logics added

--- src/main/java/com/myresume/librarymanagementsystem/library/service/LibraryService.java
+++ src/main/java/com/myresume/librarymanagementsystem/library/service/LibraryService.java
@@ -1,4 +1,36 @@
 package com.myresume.librarymanagementsystem.library.service;
 
+import com.myresume.librarymanagementsystem.exception.NotFoundException;
+import com.myresume.librarymanagementsystem.library.model.Library;
+import com.myresume.librarymanagementsystem.library.repository.LibraryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class LibraryService {
+    private final LibraryRepository libraryRepository;
+
+    public LibraryService(LibraryRepository libraryRepository) {
+        this.libraryRepository = libraryRepository;
+    }
+
+    public List<Library> getLibraries() {
+        return libraryRepository.findAll();
+    }
+
+    public Library getLibraryById(int id) {
+        return libraryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Member with id " + id + " not Found"));
+    }
+
+    public Library saveLibrary(Library library){
+        return libraryRepository.save(library);
+    }
+
+    public String deleteLibraryById(int id){
+        libraryRepository.deleteById(id);
+        return "Library with code id = " + id + " removed!";
+    }
+
 }
