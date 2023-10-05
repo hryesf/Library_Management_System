41f5529e473a37792b8dbc9cac3f5418cd702473
hryesf <houriye.esfahanian@gmail.com>
2023-10-05 16:35:48 +0330

    Employee entity with attributes and some constraints added

--- src/main/java/com/myresume/librarymanagementsystem/employee/model/Employee.java
+++ src/main/java/com/myresume/librarymanagementsystem/employee/model/Employee.java
@@ -1,25 +1,52 @@
 package com.myresume.librarymanagementsystem.employee.model;
 
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import java.util.Date;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
 public class Employee {
     @Id
-    private int EMP_ID;
-    private final String EMP_NAME;
-    private final String EMP_LASTNAME;
-    private final String EMP_NATIONALCODE;
-    private int EMP_GENDER;
-    private Date EMP_BOD;
-    private String EMP_EMAIL;
-    private String EMP_MOBILE;
-    private String EMP_TEL;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int emp_id;
+
+    @NotBlank(message = "Name must be not empty!")
+    private String emp_name;
+
+    @NotBlank(message = "Last name must be not empty!")
+    private String emp_lastName;
+
+    @NotBlank(message = "National Code must be not empty!")
+    @Digits(integer = 10, fraction = 0, message = "Member National Code must be a number with at most 10 digits")
+    private String emp_nationalCode;
+
+    @Digits(integer = 1, fraction = 0, message = "Please just enter one number 1/0")
+    private int emp_gender;
+
+    @Past(message = "the input date for Birth Date is not valid! it should belong to past!")
+    private Date emp_bod;
+
+    @Email
+    private String emp_email;
+
+    @Digits(integer = 12, fraction = 0, message = "The phone nu,ber is not correct!")
+    private String emp_mobile;
+
+    @Digits(integer = 1, fraction = 1, message = "Please just enter one number 1/0")
+    private int emp_isHired;
+
+    private int emp_library_id;
+
+    /*private String EMP_TEL;
     private String EMP_ADDRESS;
     private Date EMP_ENDDATE;
     private String EMP_DESCRIPTION;
-    private int EMP_EDUCATION_LEVEL;
+    private int EMP_EDUCATION_LEVEL;*/
 }
