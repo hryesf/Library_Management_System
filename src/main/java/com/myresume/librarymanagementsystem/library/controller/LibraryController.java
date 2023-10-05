7921c2bf862019b5cb2751f60469fa55e272bc2c
hryesf <houriye.esfahanian@gmail.com>
2023-10-05 18:02:01 +0330

    REST API methods for create, read, update and delete (CRUD) of Libraries added

---
+++ src/main/java/com/myresume/librarymanagementsystem/library/controller/LibraryController.java
@@ -0,0 +1,39 @@
+package com.myresume.librarymanagementsystem.library.controller;
+
+import com.myresume.librarymanagementsystem.library.model.Library;
+import com.myresume.librarymanagementsystem.library.service.LibraryService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/libraries")
+public class LibraryController {
+
+    private final LibraryService libraryService;
+
+    public LibraryController(LibraryService libraryService) {
+        this.libraryService = libraryService;
+    }
+
+    @GetMapping
+    List<Library> getLibraries() {
+        return libraryService.getLibraries();
+    }
+
+    @GetMapping(path = "/{library_id}")
+    Library getLibraryById(@PathVariable("library_id") int id) {
+        return libraryService.getLibraryById(id);
+    }
+
+    @PostMapping
+    String saveLibrary(@Valid @RequestBody Library library) {
+        return libraryService.saveLibrary(library).toString() + "\nSaved in database";
+    }
+
+    @DeleteMapping(path = "/{library_id}")
+    String deleteLibraryById(@PathVariable("library_id") int id){
+        return libraryService.deleteLibraryById(id);
+    }
+}
