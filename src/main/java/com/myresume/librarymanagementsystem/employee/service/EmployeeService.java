c91cb1af076cb07fa14e2cfb13ebec74e79d3da0
hryesf <houriye.esfahanian@gmail.com>
2023-10-05 16:36:13 +0330

    Employee methods and logics added

--- src/main/java/com/myresume/librarymanagementsystem/employee/service/EmployeeService.java
+++ src/main/java/com/myresume/librarymanagementsystem/employee/service/EmployeeService.java
@@ -1,4 +1,38 @@
 package com.myresume.librarymanagementsystem.employee.service;
 
+import com.myresume.librarymanagementsystem.employee.model.Employee;
+import com.myresume.librarymanagementsystem.employee.repository.EmployeeRepository;
+import com.myresume.librarymanagementsystem.exception.NotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
 public class EmployeeService {
+
+    final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployee(int id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("employee with id " + id + " not Found"));
+    }
+
+    public String deleteEmployeeById(int id) {
+        employeeRepository.deleteById(id);
+        return "employee with code " + id + " removed";
+    }
+
+
 }
