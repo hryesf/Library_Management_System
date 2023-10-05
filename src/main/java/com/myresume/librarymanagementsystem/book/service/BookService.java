74664e704be178fea69ea9d8072989cf31f7ac47
hryesf <houriye.esfahanian@gmail.com>
2023-10-05 18:42:49 +0330

    Book methods and logics added

---
+++ src/main/java/com/myresume/librarymanagementsystem/book/service/BookService.java
@@ -0,0 +1,33 @@
+package com.myresume.librarymanagementsystem.book.service;
+
+import com.myresume.librarymanagementsystem.book.model.Book;
+import com.myresume.librarymanagementsystem.book.repository.BookRepository;
+import com.myresume.librarymanagementsystem.exception.NotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(int id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Member with id " + id + " not Found"));
+    }
+
+    public Book saveBook(Book book){
+        return bookRepository.save(book);
+    }
+    
+
+}
