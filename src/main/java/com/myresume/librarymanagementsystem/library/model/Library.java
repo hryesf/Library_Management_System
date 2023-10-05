50396585ffbe4b9e44ebeeb1fa86e2ce9f676543
hryesf <houriye.esfahanian@gmail.com>
2023-10-05 18:02:36 +0330

    Library Entity with its attributes and validations added

--- src/main/java/com/myresume/librarymanagementsystem/library/model/Library.java
+++ src/main/java/com/myresume/librarymanagementsystem/library/model/Library.java
@@ -1,16 +1,34 @@
 package com.myresume.librarymanagementsystem.library.model;
 
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table
 public class Library {
+
     @Id
-    int LIBRARY_ID ;
-    String LIBRARY_NAME;
-    String LIBRARY_ADDRESS;
-    int LIBRARY_REGION;
-    int LIBRARY_TEL;
+    @SequenceGenerator(initialValue = 100,name = "number")
+    @GeneratedValue(strategy = GenerationType.IDENTITY , generator = "number")
+    int library_id ;
+
+    @NotBlank(message = "Library Name must be not empty!")
+    String library_name;
+
+    @NotBlank(message = "Library Address must be not empty!")
+    String library_address;
+
+    @NotBlank(message = "Region of library  must be not empty!")
+    String library_region;
+
+    @NotBlank(message = "Phone Number must be not empty!")
+    @Digits(integer = 12, fraction = 0, message = "Please Enter a valid telephone number")
+    String library_tel;
 }
