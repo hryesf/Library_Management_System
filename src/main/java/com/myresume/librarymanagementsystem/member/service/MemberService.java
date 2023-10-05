0c0a6e0c06f4eac586a3a3c2ca158127961e55dd
hryesf <houriye.esfahanian@gmail.com>
2023-10-05 10:16:38 +0330

    methods and logics for Member module added

--- src/main/java/com/myresume/librarymanagementsystem/member/service/MemberService.java
+++ src/main/java/com/myresume/librarymanagementsystem/member/service/MemberService.java
@@ -1,4 +1,37 @@
 package com.myresume.librarymanagementsystem.member.service;
 
+import com.myresume.librarymanagementsystem.exception.NotFoundException;
+import com.myresume.librarymanagementsystem.member.model.Member;
+import com.myresume.librarymanagementsystem.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MemberService {
+
+    final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Member saveMember(Member member) {
+        return memberRepository.save(member);
+    }
+
+    public List<Member> getAllMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member getMember(int id) {
+        return memberRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Member with id " + id + " not Found"));
+    }
+
+    public String deleteMemberById(int id){
+        memberRepository.deleteById(id);
+        return "Member with code " + id + " removed from membership";
+    }
+
 }
