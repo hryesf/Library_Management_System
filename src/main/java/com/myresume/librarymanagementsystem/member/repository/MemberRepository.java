1997738829665635af4582bf4026fe23d0046fb1
hryesf <houriye.esfahanian@gmail.com>
2023-10-06 17:27:34 +0330

    a costume findBy method added (by NationalCode) with @Query

--- src/main/java/com/myresume/librarymanagementsystem/member/repository/MemberRepository.java
+++ src/main/java/com/myresume/librarymanagementsystem/member/repository/MemberRepository.java
@@ -2,8 +2,15 @@ package com.myresume.librarymanagementsystem.member.repository;
 
 import com.myresume.librarymanagementsystem.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
+
+    @Query("SELECT m FROM Member m WHERE m.mem_nationalCode = :nationalCode")
+    Optional<Member> findByNationalCode(@Param("nationalCode") String nationalCode);
 }
