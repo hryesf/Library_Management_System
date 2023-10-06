52b777524baa856b040d65cf8203e776e2f0585c
hryesf <houriye.esfahanian@gmail.com>
2023-10-06 17:25:00 +0330

    converting primitive data types to Wrapper classes and @SequenceGenerator and using @Column for some fields

--- src/main/java/com/myresume/librarymanagementsystem/member/entity/Member.java
+++ src/main/java/com/myresume/librarymanagementsystem/member/entity/Member.java
@@ -11,44 +11,50 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+@Entity(name = "Member")
 @Table
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int mem_id;
+    @SequenceGenerator(name = "member_id_sequence", sequenceName = "member_id_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_id_sequence")
+    private Integer mem_id;
 
     @NotBlank(message = "Name must be not empty!")
+    @Column(columnDefinition = "varchar(20) default 'unknown'")
     private String mem_name;
 
     @NotBlank(message = "Last name must be not empty!")
+    @Column(columnDefinition = "varchar(50) default 'unknown'")
     private String mem_lastName;
 
     @NotBlank(message = "National Code must be not empty!")
     @Digits(integer = 10, fraction = 0, message = "Member National Code must be a number with at most 10 digits")
+    @Column(columnDefinition = "varchar(20)", unique = true)
     private String mem_nationalCode;
 
     @Digits(integer = 1, fraction = 0, message = "Please just enter one number 1/0")
-    private int mem_gender;
+    private Integer mem_gender;
+
     @Past(message = "the input date for Birth Date is not valid! it should belong to past!")
     private Date mem_bod;
 
     @Email(message = "the input Email is not valid!")
+    @Column(columnDefinition = "varchar(50)", unique = true)
     private String mem_email;
 
     @Digits(integer = 12, fraction = 0, message = "The phone nu,ber is not correct!")
     private String mem_mobile;
 
     @Digits(integer = 1, fraction = 0, message = "Please just enter one number 1/0")
-    private int mem_isActive;
+    private Integer mem_isActive;
 
-    private int mem_registrar_id;
+    private Integer mem_registrar_id;
 
     /*private String MEM_TEL;
     private String MEM_ADDRESS;
     private Date MEM_REGISTERED_DATE;
     private String MEM_DESCRIPTION;
-    private int MEM_STATUS_ID;
-    private int MEM_EDUCATION_LEVEL_ID;*/
+    private Integer MEM_STATUS_ID;
+    private Integer MEM_EDUCATION_LEVEL_ID;*/
 }
