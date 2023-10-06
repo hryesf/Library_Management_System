36b4584c7f1472631d42b8e5fa227fe080466cb1
hryesf <houriye.esfahanian@gmail.com>
2023-10-06 17:23:30 +0330

    converting primitive data types to Wrapper classes and @SequenceGenerator and using @Column for some fields

--- src/main/java/com/myresume/librarymanagementsystem/employee/entity/Employee.java
+++ src/main/java/com/myresume/librarymanagementsystem/employee/entity/Employee.java
@@ -10,43 +10,48 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+@Entity(name = "Employee")
 @Table
 public class Employee {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int emp_id;
+    @SequenceGenerator(name = "employee_id_seq", sequenceName = "employee_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employee_id_seq")
+    private Integer emp_id;
 
     @NotBlank(message = "Name must be not empty!")
+    @Column(columnDefinition = "varchar(20) default 'unknown'")
     private String emp_name;
 
     @NotBlank(message = "Last name must be not empty!")
+    @Column(columnDefinition = "varchar(50) default 'unknown'")
     private String emp_lastName;
 
     @NotBlank(message = "National Code must be not empty!")
     @Digits(integer = 10, fraction = 0, message = "Member National Code must be a number with at most 10 digits")
+    @Column(columnDefinition = "varchar(20)", unique = true)
     private String emp_nationalCode;
 
     @Digits(integer = 1, fraction = 0, message = "Please just enter one number 1/0")
-    private int emp_gender;
+    private Integer emp_gender;
 
     @Past(message = "the input date for Birth Date is not valid! it should belong to past!")
     private Date emp_bod;
 
-    @Email
+    @Email(message = "the input Email is not valid!")
+    @Column(columnDefinition = "varchar(50)", unique = true)
     private String emp_email;
 
     @Digits(integer = 12, fraction = 0, message = "The phone nu,ber is not correct!")
     private String emp_mobile;
 
     @Digits(integer = 1, fraction = 1, message = "Please just enter one number 1/0")
-    private int emp_isHired;
+    private Integer emp_isHired;
 
-    private int emp_library_id;
+    private Integer emp_library_id;
 
     /*private String EMP_TEL;
     private String EMP_ADDRESS;
     private Date EMP_ENDDATE;
     private String EMP_DESCRIPTION;
-    private int EMP_EDUCATION_LEVEL;*/
+    private Integer EMP_EDUCATION_LEVEL;*/
 }
