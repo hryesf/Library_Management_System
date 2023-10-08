df1dce4a8ef9df175b43727a5315310ae22a80f3
hryesf <houriye.esfahanian@gmail.com>
2023-10-08 18:52:30 +0330

    All settings for Book entity relationships(to category, author and publisher) added and realtioship related to LibraryBook and MemberBook classes

--- src/main/java/com/myresume/librarymanagementsystem/book/entity/Book.java
+++ src/main/java/com/myresume/librarymanagementsystem/book/entity/Book.java
@@ -1,11 +1,18 @@
 package com.myresume.librarymanagementsystem.book.entity;
 
+import com.myresume.librarymanagementsystem.bookauthor.entity.BookAuthor;
+import com.myresume.librarymanagementsystem.bookcategory.entity.BookCategory;
+import com.myresume.librarymanagementsystem.bookpublisher.entity.BookPublisher;
+import com.myresume.librarymanagementsystem.jointables.librarybook.LibraryBook;
+import com.myresume.librarymanagementsystem.jointables.memberbook.MemberBook;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -26,7 +33,22 @@ public class Book {
     private Integer book_isBorrowed;
 
     private Integer book_edition;
-    private Integer book_category_id;
-    private Integer book_author_id;
-    private Integer book_publisher_id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_author_id", referencedColumnName = "boauth_id")
+    private BookAuthor book_author_id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_category_id", referencedColumnName = "bocat_id")
+    private BookCategory book_category_id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_publisher_id", referencedColumnName = "bopub_id")
+    private BookPublisher book_publisher_id;
+
+    @OneToMany(mappedBy = "book")
+    private List<LibraryBook> libraryBookList;
+
+    @OneToMany(mappedBy = "book")
+    private List<MemberBook> memberBookList;
 }
