d3ad44b6c38c6c5805d2c6ace975f294e2c4acd9
hryesf <houriye.esfahanian@gmail.com>
2023-10-12 17:58:37 +0330

    Adding Test codes for MemberService and MemberRepository classes

---
+++ src/test/java/com/myresume/librarymanagementsystem/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,103 @@
+package com.myresume.librarymanagementsystem.member.repository;
+
+import com.myresume.librarymanagementsystem.gender.entity.Gender;
+import com.myresume.librarymanagementsystem.member.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest(properties = {"spring.jpa.properties.javax.persistence.validation.mode=none"})
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository underTest;
+
+    @Test
+    void itShouldSelectMemberByNationalCode() {
+        // Given
+        Member member = new Member("Ali",
+                "Naji",
+                "0062498125",
+                new Gender("male"),
+                "ali.naji@gmail.com",
+                1);
+
+        // When
+        underTest.save(member);
+
+        // Then
+        Optional<Member> optionalMember = underTest.findByNationalCode("0062498125");
+        assertThat(optionalMember)
+                .isPresent()
+                .hasValueSatisfying(c -> {
+                    /*assertThat(c.getMem_name()).isEqualTo("Ali");
+                    assertThat(c.getMem_lastName()).isEqualTo("Naji");
+                    assertThat(c.getMem_nationalCode()).isEqualTo("0062498125");
+                    assertThat(c.getMem_email()).isEqualTo("ali.naji@gmail.com");
+                    assertThat(c.getMem_gender_id().getGender_name()).isEqualTo("male");
+                    assertThat(c.getMem_isActive()).isEqualTo(1);*/
+                    assertThat(c).isEqualTo(member);
+                });
+    }
+
+    @Test
+    void itNotShouldSelectMemberByNationalCodeWhenCodeDoesNotExists() {
+        // Given
+        String nationalCode = "0062498125";
+
+        // When
+        Optional<Member> optionalMember = underTest.findByNationalCode(nationalCode);
+
+        // Then
+        assertThat(optionalMember).isNotPresent();
+    }
+
+     // for these tests named like "itShouldNotSaveMemberWhen...IsNull"
+     // you should put the column in entity class nullable in @column annotation
+    @Test
+    void itShouldNotSaveMemberWhenLastNameIsNull() {
+        // Given
+        String nationalCode = "0062498125";
+        Member member = new Member("Ali",
+                null,
+                nationalCode,
+                new Gender("male"),
+                "ali.naji@gmail.com",
+                1);
+
+        // When
+        // Then
+        assertThatThrownBy(() -> underTest.save(member))
+                .hasMessageContaining(
+                        "not-null property references a null or transient value : com.myresume.librarymanagementsystem.member.entity.Member.mem_lastName")
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    void itShouldNotSaveMemberWhenNationalCodeIsNull() {
+
+        // Given
+        Member member = new Member("Ali",
+                "Naji",
+                null,
+                new Gender("male"),
+                "ali.naji@gmail.com",
+                1);
+
+        // When
+        // Then
+
+        // for finding the correct exception
+        // assertThat(underTest.save(member));
+        assertThatThrownBy(() -> underTest.save(member))
+                .hasMessageContaining(
+                        "not-null property references a null or transient value : com.myresume.librarymanagementsystem.member.entity.Member.mem_nationalCode")
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+}
---
+++ src/test/java/com/myresume/librarymanagementsystem/member/service/MemberServiceTest.java
@@ -0,0 +1,116 @@
+package com.myresume.librarymanagementsystem.member.service;
+
+import com.myresume.librarymanagementsystem.gender.entity.Gender;
+import com.myresume.librarymanagementsystem.member.entity.Member;
+import com.myresume.librarymanagementsystem.member.repository.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+
+class MemberServiceTest {
+
+    private final MemberRepository memberRepository = mock(MemberRepository.class);
+    private MemberService underTest;
+
+    @Captor
+    private ArgumentCaptor<Member> memberArgumentCaptor;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new MemberService(memberRepository);
+    }
+
+    @Test
+    void itShouldSaveNewMember() {
+        // Given a national code
+        String nationalCode = "0062490000";
+        Member member = new Member("shima",
+                "sadeghi",
+                nationalCode,
+                new Gender("female"),
+                "shima.sadeghi@gmail.com",
+                1);
+        // When
+        underTest.registrationNewMember(member);
+        memberArgumentCaptor = ArgumentCaptor.forClass(Member.class);
+
+        // ... No member with national code passed
+        given(memberRepository.findByNationalCode(nationalCode)).willReturn(Optional.empty());
+
+        // Then
+        then(memberRepository).should().save(memberArgumentCaptor.capture());
+        Member memberArgumentCaptorValue = memberArgumentCaptor.getValue();
+        assertThat(memberArgumentCaptorValue).isEqualTo(member);
+    }
+
+    @Test
+    void itShouldNotSaveNewMemberWhenCustomerExists() {
+        // Given a national code
+        String nationalCode = "0062490000";
+        Member member = new Member("shima",
+                "sadeghi",
+                nationalCode,
+                new Gender("female"),
+                "shima.sadeghi@gmail.com",
+                1);
+
+        // ... An existing Member is returned
+        given(memberRepository.findByNationalCode(nationalCode)).willReturn(Optional.of(member));
+
+        // When
+        underTest.registrationNewMember(member);
+
+        // Then
+
+        // - way 1
+        then(memberRepository).should(never()).save(any());
+
+        // - way 2
+        /*then(memberRepository).should().findByNationalCode(nationalCode);
+        then(memberRepository).shouldHaveNoInteractions();*/
+
+    }
+
+    @Test
+    void itShouldThrownExceptionWhenNationalCodeIsTaken() {
+        // Given a national code
+        String nationalCode = "0062490000";
+
+        Member member = new Member("shima",
+                "sadeghi",
+                nationalCode,
+                new Gender("female"),
+                "shima.sadeghi@gmail.com",
+                1);
+
+        Member member2 = new Member("sara",
+                "pooya",
+                nationalCode,
+                new Gender("female"),
+                "sara.pooya@gmail.com",
+                1);
+
+        given(memberRepository.findByNationalCode(nationalCode)).willReturn(Optional.of(member2));
+
+        // When
+        // Then
+        assertThatThrownBy(() -> underTest.registrationNewMember(member))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("National Code [" + nationalCode + "] is taken");
+        // Finally
+        then(memberRepository).should(never()).save(any(Member.class));
+
+
+    }
+}
