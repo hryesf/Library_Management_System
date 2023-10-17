62f0a04c2e24a81066316897f4bb8b5a2e12d0c7
hryesf <houriye.esfahanian@gmail.com>
2023-10-17 08:03:46 +0330

    convert data type of donatedBookList and borrowedBookList from list to set and add customized toString()

--- src/main/java/com/myresume/librarymanagementsystem/book/entity/Book.java
+++ src/main/java/com/myresume/librarymanagementsystem/book/entity/Book.java
@@ -3,16 +3,18 @@ package com.myresume.librarymanagementsystem.book.entity;
 import com.myresume.librarymanagementsystem.bookauthor.entity.BookAuthor;
 import com.myresume.librarymanagementsystem.bookcategory.entity.BookCategory;
 import com.myresume.librarymanagementsystem.bookpublisher.entity.BookPublisher;
-import com.myresume.librarymanagementsystem.jointables.librarybook.LibraryBook;
-import com.myresume.librarymanagementsystem.jointables.memberbook.MemberBook;
+import com.myresume.librarymanagementsystem.jointables.borrowedbook.entity.BorrowedBook;
+import com.myresume.librarymanagementsystem.jointables.donatedbook.entity.DonatedBook;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -26,6 +28,7 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_id_seq")
     private Integer book_id;
 
+    @NaturalId
     @NotBlank(message = "Book Title can not be empty!")
     private String book_title;
 
@@ -49,9 +52,25 @@ public class Book {
             foreignKey = @ForeignKey( name = "book_publisher_id_fk"))
     private BookPublisher book_publisher_id;
 
-    @OneToMany(mappedBy = "book")
-    private List<LibraryBook> libraryBookList;
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<DonatedBook> donatedBookList = new HashSet<>();
 
-    @OneToMany(mappedBy = "book")
-    private List<MemberBook> memberBookList;
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<BorrowedBook> borrowedBookList = new HashSet<>();
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "book_id=" + book_id +
+                ", book_title='" + book_title + '\'' +
+                ", book_isBorrowed=" + book_isBorrowed +
+                ", book_edition=" + book_edition +
+                ", book_author_id=" + book_author_id +
+                ", book_category_id=" + book_category_id +
+                ", book_publisher_id=" + book_publisher_id +
+                ", donatedBookList=" + donatedBookList +
+                /*", borrowedBookList=" + borrowedBookList +*/
+                ", borrowedBookList=" + (borrowedBookList != null ? borrowedBookList.size() : 0) +
+                '}';
+    }
 }
