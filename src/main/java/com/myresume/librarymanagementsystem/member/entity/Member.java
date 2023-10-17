e0496b061eaeaf8574ddb34e5c3beb86fbb37810
hryesf <houriye.esfahanian@gmail.com>
2023-10-17 07:58:24 +0330

    convert data type of memberLibraryList and memberBookList from list to set and add customized toString()

--- src/main/java/com/myresume/librarymanagementsystem/member/entity/Member.java
+++ src/main/java/com/myresume/librarymanagementsystem/member/entity/Member.java
@@ -2,16 +2,19 @@ package com.myresume.librarymanagementsystem.member.entity;
 
 import com.myresume.librarymanagementsystem.employee.entity.Employee;
 import com.myresume.librarymanagementsystem.gender.entity.Gender;
-import com.myresume.librarymanagementsystem.jointables.memberbook.MemberBook;
-import com.myresume.librarymanagementsystem.jointables.memberlibrary.MemberLibrary;
+import com.myresume.librarymanagementsystem.jointables.borrowedbook.entity.BorrowedBook;
+import com.myresume.librarymanagementsystem.jointables.membership.entity.Membership;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Data
 @AllArgsConstructor
@@ -33,12 +36,13 @@ public class Member {
     @Column(columnDefinition = "varchar(50) default 'unknown'", nullable = false)
     private String mem_lastName;
 
+    @NaturalId
     @NotBlank(message = "National Code must be not empty!")
     @Digits(integer = 10, fraction = 0, message = "Member National Code must be a number with at most 10 digits")
     @Column(columnDefinition = "varchar(20)", unique = true, nullable = false)
     private String mem_nationalCode;
 
-    @ManyToOne(cascade = CascadeType.PERSIST)
+    @ManyToOne
     @JoinColumn(name = "mem_gender_id", referencedColumnName = "gender_id",
             foreignKey = @ForeignKey( name = "member_gender_id_fk"))
     private Gender mem_gender_id;
@@ -62,11 +66,11 @@ public class Member {
     private Employee employee;
 
 
-    @OneToMany(mappedBy = "member")
-    private List<MemberLibrary> memberLibraryList;
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Membership> membershipList = new HashSet<>();
 
-    @OneToMany(mappedBy = "member")
-    private List<MemberBook> memberBookList;
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<BorrowedBook> borrowedBookList = new HashSet<>();
 
     public Member(String mem_name, String mem_lastName, String mem_nationalCode, Gender mem_gender_id, String mem_email, Integer mem_isActive) {
         this.mem_name = mem_name;
@@ -77,7 +81,24 @@ public class Member {
         this.mem_isActive = mem_isActive;
     }
 
-
+    @Override
+    public String toString() {
+        return "Member{" +
+                "mem_id=" + mem_id +
+                ", mem_name='" + mem_name + '\'' +
+                ", mem_lastName='" + mem_lastName + '\'' +
+                ", mem_nationalCode='" + mem_nationalCode + '\'' +
+                ", mem_gender_id=" + mem_gender_id +
+                ", mem_bod=" + mem_bod +
+                ", mem_email='" + mem_email + '\'' +
+                ", mem_mobile='" + mem_mobile + '\'' +
+                ", mem_isActive=" + mem_isActive +
+                ", employee=" + employee +
+                ", membershipList=" + membershipList +
+                //", borrowedBookList=" + borrowedBookList +
+                ", borrowedBookList=" + (borrowedBookList != null ? borrowedBookList.size() : 0) +
+                '}';
+    }
 
     /*private String MEM_TEL;
     private String MEM_ADDRESS;
