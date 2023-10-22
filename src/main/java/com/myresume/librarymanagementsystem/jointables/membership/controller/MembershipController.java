3c14c35aea13662d6e294b0b8484d04d7bb04d85
hryesf <houriye.esfahanian@gmail.com>
2023-10-22 21:56:33 +0330

    MembershipController added

---
+++ src/main/java/com/myresume/librarymanagementsystem/jointables/membership/controller/MembershipController.java
@@ -0,0 +1,33 @@
+package com.myresume.librarymanagementsystem.jointables.membership.controller;
+
+import com.myresume.librarymanagementsystem.jointables.membership.entity.Membership;
+import com.myresume.librarymanagementsystem.jointables.membership.entity.MembershipId;
+import com.myresume.librarymanagementsystem.jointables.membership.service.MembershipService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/memberships")
+public class MembershipController {
+    private final MembershipService membershipService;
+
+    public MembershipController(MembershipService membershipService) {
+        this.membershipService = membershipService;
+    }
+
+
+    @GetMapping
+    public String getAllMemberships() {
+        return membershipService.getAllMemberships().toString();
+    }
+
+    @PostMapping("/{member_id}/{library_id}")
+    public String startMembership(@PathVariable Integer member_id , @PathVariable Integer library_id) {
+        Membership membership = membershipService.startMembership(member_id, library_id);
+        return membership.toString();
+    }
+
+    @DeleteMapping("/{id}")
+    public void endMembership(@PathVariable MembershipId id) {
+        membershipService.endMembership(id);
+    }
+}
