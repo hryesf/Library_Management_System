4df0bda93b190341c3e2c811a6e05a3134ce055a
hryesf <houriye.esfahanian@gmail.com>
2024-03-29 20:49:45 +0330

    add SecurityConfig

---
+++ src/main/java/com/myresume/librarymanagementsystem/securityconfiguration/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.myresume.librarymanagementsystem.securityconfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Deprecated
+@EnableWebSecurity
+public class SecurityConfig {
+    /*@Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }*/
+}
